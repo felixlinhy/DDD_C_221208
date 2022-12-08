1b63e80b24a1f418eaf023d793f8464037bf862d
FELIX LIN <felixlin@taifon.com.tw>
2022-12-08 16:58:55 +0800

    Add Foreign key

--- lab4_ef_core/Model/Blog.cs
+++ lab4_ef_core/Model/Blog.cs
@@ -3,4 +3,5 @@ public class Blog
 {
     public int BlogId {get; set;}
     public string? Url {get; set;}
+    public List<Post> Posts {get;} = new();
 }
--- lab4_ef_core/Program.cs
+++ lab4_ef_core/Program.cs
@@ -4,9 +4,16 @@
 using var db= new BlogContext();
 Console.WriteLine($"目前資料庫的路徑是:{db.DbPath}");
 Console.WriteLine("新增數筆資料");
-db.Add(new Blog {Url="https://www.uuu.com.tw/"});
-db.Add(new Blog {Url="https://www.tibame.com/"});
-db.Add(new Blog {Url="https://hahow.in/"});
+//db.Add(new Blog {Url="https://www.uuu.com.tw/"});
+//db.Add(new Blog {Url="https://www.tibame.com/"});
+//db.Add(new Blog {Url="https://hahow.in/"});
+
+Console.WriteLine("新增Post資料");
+var blog = db.Blogs!.OrderBy(b=> b.BlogId).Last();
+blog.Url="https://www.cwb.gov.tw";
+blog.Posts.Add(new Post{Title="urgent",Content="Typhoon is coming"});
+blog.Posts.Add(new Post{Title="urgent",Content="Earthquake last night"});
+
 db.SaveChanges();
 
 Console.WriteLine("讀回資料");
@@ -14,6 +21,11 @@ var blogs = db.Blogs!.OrderBy(b => b.BlogId);
 foreach (var b in blogs)
 {
     Console.WriteLine($"{b.BlogId},{b.Url}");
+    foreach (var p in b.Posts)
+    {
+        Console.WriteLine($"*{p.Title}/{p.Content}");
+    }
+    
 }
 
 
