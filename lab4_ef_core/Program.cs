caa5dad5e251f7e793be4d36b68f4a27830832a6
FELIX LIN <felixlin@taifon.com.tw>
2022-12-08 16:01:24 +0800

    使用EFCore

--- lab4_ef_core/Program.cs
+++ lab4_ef_core/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Model;
+
+//取得資料庫
+using var db= new BlogContext();
+Console.WriteLine($"目前資料庫的路徑是:{db.DbPath}");
+Console.WriteLine("新增數筆資料");
+db.Add(new Blog {Url="https://www.uuu.com.tw/"});
+db.Add(new Blog {Url="https://www.tibame.com/"});
+db.Add(new Blog {Url="https://hahow.in/"});
+db.SaveChanges();
+
+Console.WriteLine("讀回資料");
+var blogs = db.Blogs!.OrderBy(b => b.BlogId);
+foreach (var b in blogs)
+{
+    Console.WriteLine($"{b.BlogId},{b.Url}");
+}
+
+
