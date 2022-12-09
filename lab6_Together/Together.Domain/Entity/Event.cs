48b07b645f8a0ed530117d05c4e8c749b0be0ac4
FELIX LIN <felixlin@taifon.com.tw>
2022-12-09 17:14:03 +0800

    event

--- lab6_Together/Together.Domain/Entity/Event.cs
+++ lab6_Together/Together.Domain/Entity/Event.cs
@@ -1,8 +1,8 @@
 namespace Together.Domain.Entity;
 
 public class EventAddResult{
-    public Guid Id {get; set}= Guid.NewGuid();
-    public string? Name {get; set;};
+    public Guid Id {get; set;}= Guid.NewGuid();
+    public string? Name {get; set;} = null;
     public string? Coordinator {get; set;} = null;
     public string? Place {get;set;}=null;
     public float Lat;
