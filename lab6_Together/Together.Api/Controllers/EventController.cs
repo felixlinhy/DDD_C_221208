3de8e88f9c0b86cf9920efb27acaa6c087a5be31
FELIX LIN <felixlin@taifon.com.tw>
2022-12-09 15:11:38 +0800

    Request response Ready

--- lab6_Together/Together.Api/Controllers/EventController.cs
+++ lab6_Together/Together.Api/Controllers/EventController.cs
@@ -1,18 +1,30 @@
 namespace Together.Api.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using Together.Contract.Controller;
+
 [ApiController]
 [Route("api")]
 public class EventController:ControllerBase
 {
-    [Route("add-event")]
-    public IActionResult AddEvent()
+    [HttpPost("add-event")]
+    public IActionResult AddEvent(AddEventRequest request)
     {
-        return Ok();
+        AddEventResponse res = new AddEventResponse(
+            Guid.NewGuid(),request.name,
+            request.coordinator,request.place,request.lat,
+            request.lng,request.fee
+        );
+        return Ok(res);
     }
-    [Route("query-event")]
-    public IActionResult QueryEvent()
+    [HttpPost("query-event")]
+    public IActionResult QueryEvent(QueryEventRequest request)
     {
-        return Ok();
+        QueryEventResponse res = new QueryEventResponse(
+            "name","coord","tpe",0.5f,0.5f,300
+        );
+        QueryEventResponse[] events=new QueryEventResponse[] {res,res,res};
+
+        return Ok(events);
     }
 
 }
