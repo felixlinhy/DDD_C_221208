e6315d9ab4cf345e043650f14600d475903db23a
FELIX LIN <felixlin@taifon.com.tw>
2022-12-09 16:07:20 +0800

    Complete Add request

--- lab6_Together/Together.Api/Controllers/EventController.cs
+++ lab6_Together/Together.Api/Controllers/EventController.cs
@@ -1,24 +1,35 @@
 namespace Together.Api.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Together.Contract.Controller;
+using Together.Application.Services;
 
 [ApiController]
 [Route("api")]
 public class EventController:ControllerBase
 {
+    private readonly IEventService eventService;
+    public EventController(IEventService eventService)
+    {
+        this.eventService = eventService;
+    }
+
     [HttpPost("add-event")]
     public IActionResult AddEvent(AddEventRequest request)
     {
-        AddEventResponse res = new AddEventResponse(
-            Guid.NewGuid(),request.name,
+        var result = eventService.add(request.name,
             request.coordinator,request.place,request.lat,
-            request.lng,request.fee
+            request.lng,request.fee);
+        AddEventResponse res = new AddEventResponse(
+            result.Id,result.name,
+            result.coordinator,result.place,result.lat,
+            result.lng,result.fee
         );
         return Ok(res);
     }
     [HttpPost("query-event")]
     public IActionResult QueryEvent(QueryEventRequest request)
     {
+        var result = eventService.query(request.lat,request.lng,request.length);
         QueryEventResponse res = new QueryEventResponse(
             "name","coord","tpe",0.5f,0.5f,300
         );
